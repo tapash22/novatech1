c5664fdd6ccabda20bcf044e35797ac4d9707632
tapash22 <tapasp263@gmail.com>
2022-01-06 12:42:57 +0600

    responsive navbar

--- src/services/Navigation.vue
+++ src/services/Navigation.vue
@@ -1,94 +1,133 @@
 <template>
   <div class="navigation">
-    <nav>
-      <div class="brand">
-        <router-link to="/home">
-        <img src="../assets/image/logo2.png" />
-        </router-link>
-        <div class="btn" @click="onClick()">
-              <i class="fa fa-bars" aria-hidden="true"></i>
-            </div>
-      </div>
-      <div class="navbar" v-if="show">
-        <ul class="ul">
-          <li><router-link to="/home">Home</router-link></li>
-          <li><router-link to="/about">About</router-link></li>
-          <li><router-link to="/gallery">Gallery</router-link></li>
-          <li>
-            <router-link to="/products"
-              >Products<i
-                class="fa fa-angle-down"
-                style="padding-left: 5px; padding-top: 5px"
-                aria-hidden="true"
-              ></i
-            ></router-link>
-            <ul>
-              <li>
-                <router-link to="/poultry"
-                  >Poultry<i class="fa fa-angle-right" aria-hidden="true"></i
+    <div class="navi">
+      <nav>
+        <div class="brand">
+          <router-link to="/home">
+            <img src="../assets/image/logo2.png" />
+          </router-link>
+          <div class="btn">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="navbar" v-if="show">
+          <ul class="ul">
+            <li><router-link to="/home">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/gallery">Gallery</router-link></li>
+            <li>
+              <router-link to="/products"
+                >Products<i
+                  class="fa fa-angle-down"
+                  style="padding-left: 5px; padding-top: 5px"
+                  aria-hidden="true"
+                ></i
+              ></router-link>
+              <ul>
+                <li>
+                  <router-link to="/poultry"
+                    >Poultry<i class="fa fa-angle-right" aria-hidden="true"></i
+                  ></router-link>
+                  <ul class="two">
+                    <li>
+                      <router-link to="/antibiotic">Antibiotic</router-link>
+                    </li>
+                    <li>
+                      <router-link to="/nutritional">Nutritional</router-link>
+                    </li>
+                    <li>
+                      <router-link to="/harbal">Harbal </router-link>
+                    </li>
+                    <li>
+                      <router-link to="/others">Others</router-link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <router-link to="/dairy"
+                    >Dairy<i class="fa fa-angle-right" aria-hidden="true"></i
+                  ></router-link>
+                </li>
+                <li>
+                  <router-link to="/aqua"
+                    >Aqua<i class="fa fa-angle-right" aria-hidden="true"></i
+                  ></router-link>
+                </li>
+              </ul>
+            </li>
+            <li><router-link to="contactus">Contact us</router-link></li>
+            <li><router-link to="/login">Login</router-link></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <div class="mini">
+      <nav>
+        <div class="brand">
+          <router-link to="/home">
+            <img src="../assets/image/logo2.png" />
+          </router-link>
+          <div class="btn" @click="onClick()">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="navbar">
+          <ul class="ul">
+            <transition-group name="slide-fade">
+              <li v-if="!show"><router-link to="/home" @click="onClick()">Home</router-link></li>
+              <li v-if="!show"><router-link to="/about" @click="onClick()">About</router-link></li>
+              <li v-if="!show"><router-link to="/gallery" @click="onClick()">Gallery</router-link></li>
+              <li v-if="!show">
+                <router-link to="/products"
+                  >Products<i
+                    class="fa fa-angle-down"
+                    style="padding-left: 5px; padding-top: 5px"
+                    aria-hidden="true"
+                  ></i
                 ></router-link>
-                <ul class="two">
-                  <li>
-                    <router-link to="/antibiotic">Antibiotic</router-link>
+                <ul>
+                  <li >
+                    <router-link to="/poultry"
+                      >Poultry<i
+                        class="fa fa-angle-right"
+                        aria-hidden="true"
+                      ></i
+                    ></router-link>
+                    <ul class="two">
+                      <li >
+                        <router-link to="/antibiotic" @click="onClick()">Antibiotic</router-link>
+                      </li>
+                      <li >
+                        <router-link to="/nutritional" @click="onClick()">Nutritional</router-link>
+                      </li>
+                      <li>
+                        <router-link to="/harbal" @click="onClick()">Harbal </router-link>
+                      </li>
+                      <li>
+                        <router-link to="/others" @click="onClick()">Others</router-link>
+                      </li>
+                    </ul>
                   </li>
                   <li>
-                    <router-link to="/nutritional">Nutritional</router-link>
+                    <router-link to="/dairy" @click="onClick()"
+                      >Dairy<i class="fa fa-angle-right" aria-hidden="true"></i
+                    ></router-link>
                   </li>
                   <li>
-                    <router-link to="/harbal">Harbal </router-link>
-                  </li>
-                  <li>
-                    <router-link to="/others">Others</router-link>
+                    <router-link to="/aqua" @click="onClick()"
+                      >Aqua<i class="fa fa-angle-right" aria-hidden="true"></i
+                    ></router-link>
                   </li>
                 </ul>
               </li>
-              <li>
-                <router-link to="/dairy"
-                  >Dairy<i class="fa fa-angle-right" aria-hidden="true"></i
-                ></router-link>
-                <!-- sub-category -->
-                <!-- <ul class="two">
-                  <li>
-                    <router-link to="">Antibiotic</router-link>
-                  </li>
-                  <li>
-                    <router-link to="">Nutritional</router-link>
-                  </li>
-                  <li>
-                    <router-link to="">Harbal </router-link>
-                  </li>
-                  <li>
-                    <router-link to="">Others</router-link>
-                  </li>
-                </ul> -->
-              </li>
-              <li>
-                <router-link to="/aqua"
-                  >Aqua<i class="fa fa-angle-right" aria-hidden="true"></i
-                ></router-link>
-                <!-- sub-category -->
-                <!-- <ul class="two">
-                  <li>
-                    <router-link to="">Antibiotic</router-link>
-                  </li>
-                  <li>
-                    <router-link to="">Nutritional</router-link>
-                  </li>
-                  <li>
-                    <router-link to="">Harbal </router-link>
-                  </li>
-                  <li>
-                    <router-link to="">Others</router-link>
-                  </li>
-                </ul> -->
-              </li>
-            </ul>
-          </li>
-          <li><router-link to="contactus">Contact us</router-link></li>
-          <li><router-link to="/login">Login</router-link></li>
-        </ul>
-      </div>
-    </nav>
+              <li v-if="!show"><router-link to="contactus" @click="onClick()">Contact us</router-link></li>
+              <li v-if="!show"><router-link to="/login" @click="onClick()">Login</router-link></li>
+            </transition-group>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -105,12 +144,12 @@ export default {
   methods: {
     onClick() {
       this.show = !this.show;
-      // if (this.showSidebar) {
-      //   this.showLink = true;
-      //   setTimeout(() => {
-      //     this.showSidebar = false;
-      //   }, 200);
-      // }
+      if (this.showSidebar) {
+        this.showLink = true;
+        setTimeout(() => {
+          this.showSidebar = false;
+        }, 200);
+      }
     },
   },
 };
@@ -122,11 +161,18 @@ export default {
   height: 130px;
   padding: 0;
   margin: 0;
-  background:#7dced1;
+  background: #7dced1;
   position: fixed;
   z-index: 1;
 }
-nav {
+.navigation .navi {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+.navi nav {
   width: 100%;
   height: 100%;
   padding: 5px;
@@ -135,7 +181,7 @@ nav {
   display: flex;
   box-shadow: 2px 2px 3px #fff;
 }
-nav .brand {
+.navi nav .brand {
   width: 30%;
   height: 130px;
   padding: 0;
@@ -143,7 +189,7 @@ nav .brand {
   display: flex;
   justify-content: center;
 }
-.brand a{
+.brand a {
   text-decoration: none;
   color: #fff;
   padding: 0;
@@ -155,7 +201,7 @@ nav .brand {
   padding: 0;
   margin: 0;
 }
-.brand .btn{
+.brand .btn {
   display: none;
 }
 nav .navbar {
@@ -182,9 +228,9 @@ nav .navbar {
   display: block;
 }
 .navbar .ul li a {
-   font-size: 1.2rem;
+  font-size: 1.2rem;
   font-weight: 900;
-  font-family: 'Oswald', sans-serif;
+  font-family: "Oswald", sans-serif;
   letter-spacing: 1px;
   text-decoration: none;
   color: #000;
@@ -250,60 +296,177 @@ nav .navbar {
   margin: 0;
   width: 200px;
 }
+.navigation .mini {
+  display: none;
+}
 
 @media only screen and (max-width: 436px) {
   .navigation {
-  width: 100%;
-  height: 130px;
-  padding: 0;
-  margin: 0;
-  background:#7dced1;
-  position: fixed;
-  z-index: 1;
-}
-nav {
+    width: 100%;
+    height: 130px;
+    padding: 0;
+    margin: 0;
+    background: #7dced1;
+    position: fixed;
+    z-index: 1;
+  }
+  .navigation .navi {
+    display: none;
+  }
+  .navigation .mini {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    display: flex;
+  }
+  .mini nav {
+    width: 100%;
+    height: auto;
+    position: fixed;
+    padding: 0;
+    margin: 0;
+  }
+  nav .brand {
+    width: 100%;
+    height: 130px;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: space-between;
+  }
+  .brand a {
+    text-decoration: none;
+    color: #fff;
+    padding: 0;
+    margin: 0;
+  }
+  .brand a img {
+    width: 170px;
+    height: 120px;
+    padding: 0;
+    margin: 0;
+  }
+  .brand .btn {
+    display: block;
+  }
+  .brand .btn i {
+    font-size: 2.5rem;
+    font-weight: 700;
+    color: #000;
+    padding: 20px;
+    margin: 15px;
+  }
+nav .navbar{
   width: 100%;
   height: 100%;
-  padding: 5px;
-  margin: 0;
-  position: relative;
-  display: flex;
-  box-shadow: 2px 2px 3px #fff;
-}
-nav .brand {
-  width: 100%;
-  height: 130px;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  justify-content: space-between
-}
-.brand a{
-  text-decoration: none;
-  color: #fff;
   padding: 0;
   margin: 0;
 }
-.brand a img {
-  width: 170px;
-  height: 120px;
-  padding: 0;
-  margin: 0;
-}
-.brand .btn{
-  display: block;
-}
-.brand .btn i{
-  font-size: 2.5rem;
-  font-weight: 700;
-  color:#000;
-  padding: 20px;
-  margin: 15px;
-}
-nav .navbar {
-  display: none;
-}
-/* .navbar .ul {
+  .navbar .ul {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    background: #7dced1;
+  }
+  .navbar .ul li {
+    width: 100%;
+    
+    list-style: none;
+    align-items: center;
+  }
+  .navbar .ul li:hover ul {
+    position: relative;
+    display: block;
+    visibility: visible;
+  }
+  .navbar .ul li a {
+    font-size: 1.2rem;
+    font-weight: 900;
+    font-family: "Oswald", sans-serif;
+    letter-spacing: 1px;
+    text-decoration: none;
+    color: #000;
+    margin: 0;
+    padding: 5px;
+    border-bottom: 2px none #fff;
+  }
+  .navbar .ul li a:hover,
+  .navbar .ul li a.router-link-exact-active {
+    border-bottom: 2px solid #fff;
+  }
+  .navbar .ul li ul {
+    visibility: hidden;
+    display: block;
+    padding: 0;
+    margin: 0;
+    background: #0b53699d;
+    margin-top: 5px;
+    z-index: 1;
+  }
+  .navbar .ul li ul li {
+    padding: 10px;
+    margin: 0;
+  }
+  .navbar .ul li ul li a {
+    font-size: 1.2rem;
+    font-weight: 500;
+    padding: 5px;
+    margin: 5px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: none;
+  }
+  .navbar .ul li ul li a:hover,
+  .navbar .ul li ul li a.router-link-exact-active {
+    border-bottom: none;
+    color: #fff;
+  }
+  .navbar .ul li ul li a i {
+    padding-top: 5px;
+    padding-left: 15px;
+    margin: 0;
+    font-size: 1.4rem;
+    font-weight: 500;
+  }
+  .navbar .ul li ul li:hover {
+    background: rgba(0, 0, 0, 0.24);
+  }
+  .navbar .ul li ul li:hover .two {
+    position: relative;
+    display: block;
+    visibility: visible;
+  }
+
+  .navbar .ul li ul li .two {
+    visibility: hidden;
+    display: block;
+    padding: 0;
+    margin: 0;
+    background: #0b53699d;
+    z-index: 1;
+  }
+  .navbar .ul li ul li .two li {
+    padding: 10px;
+    margin: 0;
+    width: 200px;
+  }
+
+  .slide-fade-enter-active {
+    transition: all 0.3s ease-in;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.3s ease-out;
+  }
+
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateX(10%);
+    opacity: 0;
+  }
+  /* .navbar .ul {
   padding: 0;
   margin: 0;
   display: flex;
